9603827dd07216c05773c3cf0bf9fd00aefe1e97
haram8009 <haram8009@gmail.com>
2024-07-06 04:33:00 +0900

    refactor: 서버 실행파일 app.js로 이름 변경, 라우팅 모듈식으로 리팩토링

--- src/app.js
+++ src/app.js
@@ -1,11 +1,10 @@
 const express = require("express");
 const app = express();
 const port = 5000;
+const indexRouter = require("./routes/index");
 
 // routes
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
+app.use("/", indexRouter);
 
 // start server
 app.listen(port, () => {
