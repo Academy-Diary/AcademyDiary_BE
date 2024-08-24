6af2418929108aef5b79421aa9d0655082f17b32
daesung <dasung56@gmail.com>
2024-08-24 20:46:28 +0900

    #7 feat: 강사/학생이 학원에 등록 API구현

--- src/controllers/registerationController.js
+++ src/controllers/registerationController.js
@@ -45,3 +45,52 @@ exports.registAcademy = asyncWrapper(async(req, res, next) => {
         }
     }
 })
+
+exports.registUser = asyncWrapper(async(req, res, next) =>{
+    const { user_id, academy_key, role } = req.body;
+
+    try {
+        //입력받은 academy_key로 academy찾기
+        const searchAcademy = await prisma.academy.findUnique({
+            where : { academy_key }
+        })
+        if(!searchAcademy){
+            return res.status(StatusCodes.NOT_FOUND).json({
+                message: '학원을 찾을 수 없습니다.'
+            });
+        }
+
+        //학원 유저 신청 목록DB에 user_id가 이미 있는지 검사
+        const checkUser = await prisma.AcademyUserRegistrationList.findUnique({
+            where : { user_id }
+        })
+        if (checkUser) {
+            return res.status(StatusCodes.CONFLICT).json({
+                message: '이미 등록요청된 유저입니다.'
+            });
+        }
+        //없다면 DB에 req.body내용 추가
+        const newUser = await prisma.AcademyUserRegistrationList.create({
+            data: {
+                user_id,
+                academy_id : searchAcademy.academy_id,
+                role,
+                status: 'INITIAL'
+            }
+        });
+
+        res.status(StatusCodes.CREATED).json({
+            message: '등록요청이 성공적으로 완료되었습니다.',
+            data: newUser
+        })
+    
+    } catch(error) {
+        if (error.code === 'P2002') { // Prisma의 unique constraint 오류 코드
+            res.status(StatusCodes.DUPLICATE_ENTRY).json({
+                message: '이미 등록요청된 유저입니다.'
+            })
+        }
+    }
+
+
+})
--- src/routes/registerationRouter.js
+++ src/routes/registerationRouter.js
@@ -5,6 +5,8 @@ const { authenticateJWT } = require('../lib/middlewares/auth.js')
 
 router.post('/request/academy', authenticateJWT, regsiterController.registAcademy);
 
+router.post('/request/user',authenticateJWT, regsiterController.registUser);
+
 // 보호된 라우트 예시
 // router.get("/protected", authenticateJWT, (req, res) => {
 //   res.json({ message: "This is a protected route", user: req.user });
