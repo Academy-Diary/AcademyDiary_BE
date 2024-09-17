8e5ef0c0f7cd051c3da03109520a33d1a2c38940
daesung <dasung56@gmail.com>
2024-09-17 16:37:59 +0900

    #32 add: expense Router,Controller추가

---
+++ src/controllers/expenseController.js
@@ -0,0 +1,5 @@
+const { asyncWrapper } = require("../lib/middlewares/async");
+const prisma = require("../lib/prisma/index");
+const { CustomError } = require("../lib/errors/customError");
+const ErrorCode = require("../lib/errors/errorCode");
+const { StatusCodes } = require("http-status-codes");
---
+++ src/routes/expenseRouter.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const { authenticateJWT } = require("../lib/middlewares/auth.js");
+const router = express.Router();
+const lectureController = require("../controllers/lectureController.js");
+
+
+
+
+module.exports = router;
