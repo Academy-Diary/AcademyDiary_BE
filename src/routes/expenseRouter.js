5658b67745dc0707df841785477f2e0cbfd5ba24
daesung <dasung56@gmail.com>
2024-09-23 13:12:31 +0900

    #32 chore: jwt미들웨어 오타수정

--- src/routes/expenseRouter.js
+++ src/routes/expenseRouter.js
@@ -4,16 +4,16 @@ const router = express.Router();
 const expenseController = require("../controllers/expenseController.js");
 
 //Class생성
-router.post("/:academy_id", authenticateJWT, expenseController.createClass);
+router.post("/:academy_id", authenticateJWT("CHIEF"), expenseController.createClass);
 
 //Class조회
-router.get("/:academy_id", authenticateJWT, expenseController.getClass);
+router.get("/:academy_id", authenticateJWT("CHIEF","TEACHER"), expenseController.getClass);
 
 //Class수정
-router.put("/:academy_id/:class_id", authenticateJWT, expenseController.updateClass);
+router.put("/:academy_id/:class_id", authenticateJWT("CHIEF"), expenseController.updateClass);
 
 //Class삭제
-router.delete("/:academy_id/:class_id", authenticateJWT, expenseController.deleteClass);
+router.delete("/:academy_id/:class_id", authenticateJWT("CHIEF"), expenseController.deleteClass);
 
 
 module.exports = router;
