9682884062ba14d94fa760cb8cdcaa6f186f7528
daesung <dasung56@gmail.com>
2024-09-23 13:05:46 +0900

    #32 fix: Class수정,삭제 엔드포인트 수정

--- src/routes/expenseRouter.js
+++ src/routes/expenseRouter.js
@@ -10,10 +10,10 @@ router.post("/:academy_id", authenticateJWT, expenseController.createClass);
 router.get("/:academy_id", authenticateJWT, expenseController.getClass);
 
 //Class수정
-router.put("/:academy_id", authenticateJWT, expenseController.updateClass);
+router.put("/:academy_id/:class_id", authenticateJWT, expenseController.updateClass);
 
 //Class삭제
-router.delete("/:class_id", authenticateJWT, expenseController.deleteClass);
+router.delete("/:academy_id/:class_id", authenticateJWT, expenseController.deleteClass);
 
 
 module.exports = router;
