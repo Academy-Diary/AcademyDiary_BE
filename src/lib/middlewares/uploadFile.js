8cb7075b7262b4ad1f67657a478d9cff44accd0b
SunwooKim11 <seonu2001@naver.com>
2024-10-16 02:56:56 +0900

    #36 add: local dir을 s3에 업로드하는 미들웨어 추가

--- src/lib/middlewares/uploadFile.js
+++ src/lib/middlewares/uploadFile.js
@@ -1,9 +1,12 @@
 const multer = require("multer");
 const path = require("path");
+const fs = require("fs");
 const { CustomError } = require("../errors/customError");
 const ErrorCode = require("../errors/errorCode");
 const { StatusCodes } = require("http-status-codes");
 const { asyncWrapper } = require("./async");
+const s3 = require("../../config/s3client");
+const { PutObjectCommand } = require("@aws-sdk/client-s3");
 
 // Multer 저장소 설정
 const profileStorage = multer.diskStorage({
@@ -22,7 +25,9 @@ const noticeStorage = multer.diskStorage({
     cb(null, path.join(__dirname, "../../../public/notice/tmp"));
   },
   filename: (req, file, cb) => {
-    file.originalname = Buffer.from(file.originalname, 'latin1').toString('utf8');
+    file.originalname = Buffer.from(file.originalname, "latin1").toString(
+      "utf8"
+    );
     cb(null, file.originalname);
   },
 });
@@ -59,4 +64,35 @@ const uploadNoticeFile = multer({
   },
 });
 
-module.exports = { uploadProfileImage, uploadNoticeFile };
+// 디렉토리 내의 모든 파일을 S3로 업로드
+async function uploadDirToS3(dirPath, bucketName, s3KeyPrefix) {
+  const files = await getFiles(dirPath);
+
+  const uploads = files.map((filePath) => {
+    const s3Key = `${s3KeyPrefix}/${path
+      .relative(dirPath, filePath)
+      .replace(/\\/g, "/")}`;
+    return s3.send(
+      new PutObjectCommand({
+        Bucket: bucketName,
+        Key: s3Key,
+        Body: fs.createReadStream(filePath),
+      })
+    );
+  });
+
+  await Promise.all(uploads);
+}
+
+// 디렉토리 내의 모든 파일을 가져오기 위한 재귀 함수
+async function getFiles(dir) {
+  const dirents = await fs.promises.readdir(dir, { withFileTypes: true });
+  const files = await Promise.all(
+    dirents.map((dirent) => {
+      const res = path.resolve(dir, dirent.name);
+      return dirent.isDirectory() ? getFiles(res) : res;
+    })
+  );
+  return Array.prototype.concat(...files);
+}
+module.exports = { uploadProfileImage, uploadNoticeFile, uploadDirToS3 };
