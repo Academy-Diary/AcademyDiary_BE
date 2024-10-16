ef28359f8b471b32251e91ce84fae7ed208942dd
SunwooKim11 <seonu2001@naver.com>
2024-10-16 21:23:29 +0900

    #36 add: s3client endpoint 추가

--- src/config/s3client.js
+++ src/config/s3client.js
@@ -7,6 +7,7 @@ const {
 
 const s3 = new S3Client({
   region: AWS_REGION,
+  endpoint: `https://s3.${AWS_REGION}.amazonaws.com`, // 명확히 리전 기반 엔드포인트 설정
   credentials: {
     accessKeyId: AWS_ACCESS_KEY_ID,
     secretAccessKey: AWS_SECRET_ACCESS_KEY,
