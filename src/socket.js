e365030ce160f80f362a77dcb621a8ba6910af96
daesung <dasung56@gmail.com>
2024-10-30 03:52:36 +0900

    #87 feat : 방생성&입장 로직 추가 & 메시지조회 추가

--- src/socket.js
+++ src/socket.js
@@ -1,30 +1,68 @@
 const { getChatDB } = require("./lib/chatDB/chatDB"); // MongoDB 인스턴스 가져오기
+const { ObjectId } = require("mongodb");
 
 module.exports = (server) => {
   const io = require("socket.io")(server);
 
   io.on("connection", (socket) => {
-    console.log("A user connected:", socket.id);
+    console.log("User connected:", socket.id);
 
-    // 채팅 메시지 전송 이벤트 처리
-    socket.on("chat message", async (msg) => {
-      console.log("message:", msg);
-      
-      // MongoDB에 메시지 저장
-      const chatDB = getChatDB();
-      await chatDB.collection("chat_content").insertOne({
-        userId: socket.id,
-        message: msg,
-        timestamp: new Date(),
+    // 방 생성 및 입장
+    socket.on("create or join room", async ({ roomId, userId }) => {
+        socket.join(roomId);
+        console.log(`User ${userId} joined room: ${roomId}`);
+  
+        // MongoDB에 방 정보 저장 (이미 존재하지 않는다면)
+        const chatDB = getChatDB();
+        const existingRoom = await chatDB.collection("chat_room").findOne({ _id: new ObjectId(roomId) });
+        if (!existingRoom) {
+          await chatDB.collection("chat_room").insertOne({
+            _id: new ObjectId(roomId),
+            members: [userId],
+            created_at: new Date(),
+          });
+        } else {
+          // 이미 존재하면 멤버 목록에 추가
+          await chatDB.collection("chat_room").updateOne(
+            { _id: new ObjectId(roomId) },
+            { $addToSet: { members: userId } } // 중복 없이 추가
+          );
+        }
+  
+        io.to(roomId).emit("user joined", { userId });
       });
 
-      // 모든 클라이언트에게 메시지 전송
-      io.emit("chat message", msg);
-    });
+    // 채팅 메시지 전송
+    socket.on("chat message", async ({ roomId, message, userId }) => {
+        const chatDB = getChatDB();
+        
+        // MongoDB에 메시지 저장
+        const chatMessage = {
+          roomId: new ObjectId(roomId),
+          userId,
+          message,
+          timestamp: new Date(),
+        };
+        await chatDB.collection("chat_content").insertOne(chatMessage);
+  
+        // 해당 방에만 메시지 전송
+        io.to(roomId).emit("chat message", chatMessage);
+      });
+
+    // 채팅방의 모든 메시지 조회
+    socket.on("get messages", async ({ roomId }) => {
+        const chatDB = getChatDB();
+        const messages = await chatDB.collection("messages")
+          .find({ roomId: new ObjectId(roomId) })
+          .sort({ timestamp: 1 })
+          .toArray();
+  
+        socket.emit("all messages", messages); // 해당 사용자에게만 전송
+      });
 
     // 연결 해제 이벤트
     socket.on("disconnect", () => {
-      console.log("User disconnected");
+      console.log("User disconnected", socket.id);
     });
   });
 };
