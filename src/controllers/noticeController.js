4d23243525a63f5816d2b1cc0bb210348645b560
SunwooKim11 <seonu2001@naver.com>
2024-11-04 16:17:20 +0900

    #36 feat: deleteNotice 유효성 검사 추가

--- src/controllers/noticeController.js
+++ src/controllers/noticeController.js
@@ -134,6 +134,7 @@ exports.deleteNotice = asyncWrapper(async (req, res, next) => {
   const lecture_id = parseInt(notice_id[1], 10);
   const notice_num = parseInt(notice_id[2], 10);
 
+  // 유효성 검사1: 값들이 존재하지 않으면 에러 처리
   if (isNaN(lecture_id) || isNaN(notice_num)) {
     return next(
       new CustomError(
@@ -144,7 +145,34 @@ exports.deleteNotice = asyncWrapper(async (req, res, next) => {
     );
   }
 
-  // NoticeFile 조회
+  // 유효성 검사2: 다른 학원의 공지를 삭제하려고 하는 경우 에러 처리
+  if (academy_id !== req.user.academy_id) {
+    return next(
+      new CustomError(
+        "해당 학원에 대한 권한이 없습니다.",
+        StatusCodes.FORBIDDEN,
+        StatusCodes.FORBIDDEN
+      )
+    );
+  }
+
+  const notice = await prisma.Notice.findFirst({
+    where: {
+      notice_id: req.params.notice_id,
+    },
+  });
+
+  // 유효성 검사3: 해당 공지사항이 존재하지 않으면 에러 처리
+  if (!notice) {
+    return next(
+      new CustomError(
+        "해당 공지사항이 존재하지 않습니다.",
+        StatusCodes.NOT_FOUND,
+        StatusCodes.NOT_FOUND
+      )
+    );
+  }
+
   const notice_files = await prisma.NoticeFile.findMany({
     where: {
       notice_id: req.params.notice_id,
