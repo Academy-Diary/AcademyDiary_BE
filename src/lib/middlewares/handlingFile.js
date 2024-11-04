49b14dcc108a7aefeddc204d039bbb25907fc860
SunwooKim11 <seonu2001@naver.com>
2024-11-04 16:11:56 +0900

    #36 fix: handlingFile 불필요 함수 삭제 및 notice_id 파싱 수정
    - 불필요한 함수를 삭제함.
    - notice_id을 params or body로 부터 받아, s3 파일 path(이름)을 만듦

--- src/lib/middlewares/handlingFile.js
+++ src/lib/middlewares/handlingFile.js
@@ -13,7 +13,7 @@ const {
 const { S3_BUCKET_NAME } = require("../../config/secret");
 const multerS3 = require("multer-s3");
 
-// Multer 저장소 설정
+// profile Multer 저장소 설정
 const profileStorage = multer.diskStorage({
   destination: (req, file, cb) => {
     cb(null, path.resolve(__dirname, `../../../public/profile/`)); // 파일 저장 경로
@@ -24,20 +24,7 @@ const profileStorage = multer.diskStorage({
   },
 });
 
-const noticeStorage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    // destination은 임시로 기본 경로로 설정
-    cb(null, path.join(__dirname, "../../../public/notice/tmp"));
-  },
-  filename: (req, file, cb) => {
-    file.originalname = Buffer.from(file.originalname, "latin1").toString(
-      "utf8"
-    );
-    cb(null, file.originalname);
-  },
-});
-
-// Multer 파일 필터 및 제한 설정
+// Multer image 파일 필터 및 제한 설정
 const imageFilter = (req, file, cb) => {
   const allowedTypes = ["image/jpeg", "image/jpg", "image/png"];
   if (!allowedTypes.includes(file.mimetype)) {
@@ -69,30 +56,25 @@ const uploadNoticeFile = multer({
     contentType: multerS3.AUTO_CONTENT_TYPE,
     acl: "public-read",
     key: (req, file, cb) => {
-      // let academy_id, lecture_id, notice_num;
-      try {
-        let notice_id;
-        if (isNaN(req.params.notice_id)) {
-          notice_id = req.body.notice_id.split("&");
-        } else {
-          notice_id = req.params.notice_id.split("&");
-        }
-        req.body.academy_id = notice_id[0];
-        req.body.lecture_id = parseInt(notice_id[1], 10);
-        req.body.notice_num = parseInt(notice_id[2], 10);
-      } catch (error) {
-        console.log(req);
-        throw new CustomError(
-          "유효한 값들을 입력해주세요.",
-          StatusCodes.BAD_REQUEST,
-          StatusCodes.BAD_REQUEST
-        );
-      } // 경로가 존재하지 않더라도 S3는 자동으로 경로 생성
+      console.log(req.params.notice_id);
+      let notice_id;
+      if (!req.params.notice_id) {
+        console.log('params 없음');
+        notice_id = req.body.notice_id.split("&");
+      } else {
+        console.log(req.params);
+        notice_id = req.params.notice_id.split("&");
+      }
+      const academy_id  = notice_id[0];
+      const lecture_id  = parseInt(notice_id[1], 10);
+      const notice_num  = parseInt(notice_id[2], 10);
+     
+      // 경로가 존재하지 않더라도 S3는 자동으로 경로 생성
       file.originalname = Buffer.from(file.originalname, "latin1").toString(
         "utf8"
       );
 
-      const s3KeyPrefix = `public/notice/${req.body.academy_id}/${req.body.lecture_id}/${req.body.notice_num}`;
+      const s3KeyPrefix = `public/notice/${academy_id}/${lecture_id}/${notice_num}`;
       const fileKey = `${s3KeyPrefix}/${file.originalname}`;
 
       cb(null, fileKey);
@@ -101,46 +83,12 @@ const uploadNoticeFile = multer({
   limits: { fileSize: 10 * 1024 * 1024 }, // 파일 크기 제한 10MB
 });
 
-// 디렉토리 내의 모든 파일을 S3로 업로드
-async function uploadDirToS3(dirPath, bucketName, s3KeyPrefix) {
-  const files = await getFiles(dirPath);
-
-  const uploads = files.map((filePath) => {
-    const s3Key = `${s3KeyPrefix}/${path
-      .relative(dirPath, filePath)
-      .replace(/\\/g, "/")}`;
-    return s3.send(
-      new PutObjectCommand({
-        Bucket: bucketName,
-        Key: s3Key,
-        Body: fs.createReadStream(filePath),
-      })
-    );
-  });
-
-  await Promise.all(uploads);
-}
-
-// 디렉토리 내의 모든 파일을 가져오기 위한 재귀 함수
-async function getFiles(dir) {
-  const dirents = await fs.promises.readdir(dir, { withFileTypes: true });
-  const files = await Promise.all(
-    dirents.map((dirent) => {
-      const res = path.resolve(dir, dirent.name);
-      return dirent.isDirectory() ? getFiles(res) : res;
-    })
-  );
-  return Array.prototype.concat(...files);
-}
-
 /**
  * S3에서 파일을 삭제하는 함수
  * @param {string} bucketName - S3 버킷 이름
  * @param {string} s3Prefix - 삭제할 파일들의 공통 경로 (폴더)
  * @param {string[]} filesToDelete - 삭제할 파일의 Key 목록 (전체 삭제 시 null 또는 빈 배열)
  */
-
-// S3에서 파일 삭제하는 함수
 async function deleteFilesFromS3(bucketName, s3Prefix, filesToDelete = null) {
   try {
     let objectsToDelete = [];
@@ -175,9 +123,9 @@ async function deleteFilesFromS3(bucketName, s3Prefix, filesToDelete = null) {
       };
       const deleteCommand = new DeleteObjectsCommand(deleteParams);
       await s3.send(deleteCommand);
-      console.log(`S3 파일 삭제 성공: ${s3Prefix}`);
+      console.log(`S3 파일 삭제 성공: ${objectsToDelete}`);
     } else {
-      console.log(`삭제할 S3 파일이 없습니다: ${s3Prefix}`);
+      console.log(`삭제할 S3 파일이 없습니다: ${objectsToDelete}`);
     }
   } catch (error) {
     console.error(`S3 파일 삭제 실패: ${error}`);
@@ -192,6 +140,5 @@ async function deleteFilesFromS3(bucketName, s3Prefix, filesToDelete = null) {
 module.exports = {
   uploadProfileImage,
   uploadNoticeFile,
-  uploadDirToS3,
   deleteFilesFromS3,
 };
