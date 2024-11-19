f4092e636cb89c98dd445f374010d32ce02af34d
SunwooKim11 <seonu2001@naver.com>
2024-11-19 22:22:11 +0900

    #117 add: get mongoClient 추가

--- src/lib/mongo/mongo.js
+++ src/lib/mongo/mongo.js
@@ -1,12 +1,12 @@
 const { MongoClient } = require("mongodb");
-const { chatDBurl } = require("../../config/secret");
+const { MONGO_DB_URL } = require("../../config/secret");
 
-let chatDB, quizDB;
+let client, chatDB, quizDB;
 
 const connectToMongo = async () => {
   try {
-    const client = await new MongoClient(chatDBurl).connect();
-    console.log("채팅DB 연결 성공");
+    client = await new MongoClient(MONGO_DB_URL).connect();
+    console.log("MongoDB 연결 성공");
     chatDB = client.db("chat");
     quizDB = client.db("Quiz");
   } catch (err) {
@@ -16,16 +16,30 @@ const connectToMongo = async () => {
 
 const getChatDB = () => {
   if (!chatDB) {
-    throw new Error("MongoDB에 연결되지 않았습니다. 먼저 connectToMongo를 호출하세요.");
+    throw new Error(
+      "MongoDB에 연결되지 않았습니다. 먼저 connectToMongo를 호출하세요."
+    );
   }
   return chatDB;
 };
 
 const getQuizDB = () => {
   if (!quizDB) {
-    throw new Error("MongoDB에 연결되지 않았습니다. 먼저 connectToMongo를 호출하세요.");
+    throw new Error(
+      "MongoDB에 연결되지 않았습니다. 먼저 connectToMongo를 호출하세요."
+    );
   }
   return quizDB;
-}
+};
 
-module.exports = { connectToMongo, getChatDB, getQuizDB };
+// MongoClient를 반환하는 함수 추가
+const getMongoClient = () => {
+  if (!client) {
+    throw new Error(
+      "MongoClient가 초기화되지 않았습니다. 먼저 connectToMongo를 호출하세요."
+    );
+  }
+  return client;
+};
+
+module.exports = { connectToMongo, getChatDB, getQuizDB, getMongoClient };
