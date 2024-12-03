79e0d079e43ffd82ef14c66ddfc58bccc0bc5ab0
SunwooKim11 <seonu2001@naver.com>
2024-12-03 20:23:52 +0900

    #141 fix: 학원에서 강사삭제 API 실행 시 강사 인원수 갱신되도록 수정

--- src/controllers/teacherController.js
+++ src/controllers/teacherController.js
@@ -3,7 +3,7 @@ const prisma = require("../lib/prisma/index");
 const { CustomError } = require("../lib/errors/customError");
 const ErrorCode = require("../lib/errors/errorCode");
 const { StatusCodes } = require("http-status-codes");
-
+const { updateHeadcount } = require("../lib/prisma/headcount");
 
 exports.deleteTeacher = asyncWrapper(async(req, res, next) => {
     const arr_user_id = req.body.user_id;
@@ -18,37 +18,48 @@ exports.deleteTeacher = asyncWrapper(async(req, res, next) => {
         )
       );
     }
-    const transactionOperations = [
-      //강사의 academy_id를 NULL로 업데이트
-      prisma.user.updateMany({
-        where: {
-          user_id: {
-            in: arr_user_id,
+    try{
+      const result = await prisma.$transaction(async (prisma) => {
+        // 1. 강사 및 관련 데이터 삭제
+        // 강사의 academy_id를 NULL로 업데이트
+        await prisma.user.updateMany({
+          where: {
+            user_id: {
+              in: arr_user_id,
+            },
           },
-        },
-        data: {
-          academy_id: null,
-        },
-      }),
-      //AcademyUserRegistrationList에서 해당 강사 행 삭제
-      prisma.AcademyUserRegistrationList.deleteMany({
-        where: {
-          user_id: {
-            in: arr_user_id,
+          data: {
+            academy_id: null,
           },
-        },
-      }),
-    ];
-    await prisma.$transaction(transactionOperations);
+        });
+        
+        // AcademyUserRegistrationList에서 해당 강사 행 삭제
+        await prisma.AcademyUserRegistrationList.deleteMany({
+          where: {
+            user_id: {
+              in: arr_user_id,
+            },
+          },
+        });
+        // Headcount 업데이트
+        return await updateHeadcount(prisma, req.user.academy_id)
+      });
 
-    return res.status(StatusCodes.OK).json({
-      message: "강사 삭제가 성공적으로 완료되었습니다.",
-      data:{
-        deletedUserIds: arr_user_id,
-        deletedCount: arr_user_id.length // 입력과 실제 삭제되는 유저 수가 같음
-      }
-    });
-  
+      return res.status(StatusCodes.OK).json({
+        message: "강사 삭제가 성공적으로 완료되었습니다.",
+        data:{
+          deletedUserIds: arr_user_id,
+          deletedCount: arr_user_id.length, // 입력과 실제 삭제되는 유저 수가 같음
+          remainedCount: result.teacher_headcount // 삭제 후 남은 강사 수
+        }
+      });
+    }catch(error) {
+      return next(new CustomError(
+        "강사 삭제 중 오류가 발생했습니다.",
+        StatusCodes.INTERNAL_SERVER_ERROR,
+        StatusCodes.INTERNAL_SERVER_ERROR
+      ));
+    }
 })
 
 exports.getTeacher = asyncWrapper(async(req, res, next) => {
