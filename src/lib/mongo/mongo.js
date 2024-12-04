9801a345755ff704adb0d418c5218de3039e3d01
daesung <dasung56@gmail.com>
2024-12-04 19:15:27 +0900

    #147 add :몽고DB auth 컬렉션함수 추가

--- src/lib/mongo/mongo.js
+++ src/lib/mongo/mongo.js
@@ -1,7 +1,7 @@
 const { MongoClient } = require("mongodb");
 const { MONGO_DB_URL } = require("../../config/secret");
 
-let client, chatDB, quizDB;
+let client, chatDB, quizDB, authDB;
 
 const connectToMongo = async () => {
   try {
@@ -9,6 +9,7 @@ const connectToMongo = async () => {
     console.log("MongoDB 연결 성공");
     chatDB = client.db("chat");
     quizDB = client.db("Quiz");
+    authDB = client.db("auth");
   } catch (err) {
     console.error("MongoDB 연결 실패", err);
   }
@@ -32,6 +33,15 @@ const getQuizDB = () => {
   return quizDB;
 };
 
+const getAuthDB = () => {
+  if (!authDB) {
+    throw new Error(
+      "MongoDB에 연결되지 않았습니다. 먼저 connectToMongo를 호출하세요."
+    );
+  }
+  return authDB;
+};
+
 // MongoClient를 반환하는 함수 추가
 const getMongoClient = () => {
   if (!client) {
@@ -42,4 +52,4 @@ const getMongoClient = () => {
   return client;
 };
 
-module.exports = { connectToMongo, getChatDB, getQuizDB, getMongoClient };
+module.exports = { connectToMongo, getChatDB, getQuizDB, getAuthDB, getMongoClient };
